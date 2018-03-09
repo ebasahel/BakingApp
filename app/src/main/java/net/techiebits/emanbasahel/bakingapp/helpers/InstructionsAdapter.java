4870ce692b4ae36cce618cad53628cad2735a2ac
emanbasahel <Eman0500821725>
2018-03-10 00:22:11 +0300

    no video placeholder (#Fixed Temporary)

--- app/src/main/java/net/techiebits/emanbasahel/bakingapp/helpers/InstructionsAdapter.java
+++ app/src/main/java/net/techiebits/emanbasahel/bakingapp/helpers/InstructionsAdapter.java
@@ -6,7 +6,9 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.google.android.exoplayer2.ui.SimpleExoPlayerView;
@@ -25,6 +27,7 @@ public class InstructionsAdapter extends RecyclerView.Adapter<InstructionsAdapte
     private SimpleExoPlayerView mPlayerView;
     private Context mContext;
     private VideoHandlerInterface videoHandler;
+    private ImageView noVideoPlaceholder;
 
     public interface VideoHandlerInterface {
         void onVideoDisplayed(String videoURL);
@@ -44,13 +47,17 @@ public class InstructionsAdapter extends RecyclerView.Adapter<InstructionsAdapte
             super(itemView);
             txtInstruction = (TextView) itemView.findViewById(R.id.txt_recipe_instruction);
             mPlayerView = (SimpleExoPlayerView) itemView.findViewById(R.id.player_view);
+            noVideoPlaceholder = (ImageView) itemView.findViewById(R.id.video_placeholder);
         }
 
         void bind(final Step step, final VideoHandlerInterface videoHandlerInterface) {
             txtInstruction.setText(step.getDescription());
             if (step.getVideoURL().isEmpty()) {
                 mPlayerView.setVisibility(View.GONE);
+                noVideoPlaceholder.setVisibility(View.VISIBLE);
             } else {
+                mPlayerView.setVisibility(View.VISIBLE);
+                noVideoPlaceholder.setVisibility(View.GONE);
                 // Initialize the Media Session.
                 ExoPlayerVideoHandler.getInstance().prepareExoPlayerForUri(mContext, Uri.parse(step.getVideoURL()), mPlayerView);
                 videoHandlerInterface.onVideoDisplayed(step.getVideoURL());
